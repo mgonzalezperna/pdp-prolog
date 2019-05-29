95891c8700264e69dd41de72d4b12c5b3e690446
Martin Gonzalez Perna <mgonzalezperna@gmail.com>
2019-05-28 21:23:37 -0300

    Agregar mas refranes.

--- clase2/compras.pl
+++ clase2/compras.pl
@@ -1,11 +1,60 @@
-compras(Persona, Producto) :-
+compra(Persona, Producto, Cantidad) :-
     dinero(Persona, Dinero),
     precio(Producto, Precio),
-    Dinero >= Precio, 
-    Cantidad = Dinero/Precio.
+    Cantidad is Dinero/Precio,
+    Dinero >= Precio. 
 
 dinero(nicolas, 200).
 dinero(javier, 1030).
+
 precio(mouse, 400).
+precio(cafe, 25).
+precio(auriculares, 250).
 
 
+
+profesion(juan, herrero).
+profesion(ana, carpintero).
+profesion(pedro, herrero).
+
+tieneCuchillo(juan, palo).
+tieneCuchillo(pedro, palo).
+tieneCuchillo(ana, palo).
+
+enCasaDeHerreroCuchilloDePalo :-
+    forall(
+        profesion(Fulano, herrero),
+        tieneCuchillo(Fulano, palo)
+    ).
+
+enCasaDeHerreroCuchilloDePalo2 :-
+    not((profesion(Fulano, herrero),
+    not(tieneCuchillo(Fulano, palo)))
+).
+
+madruga(juan).
+madruga(ana).
+loAyuda(yaveh, juan).
+loAyuda(ala, ana).
+
+esDios(ala).
+esDios(yaveh).
+
+alQueMadrugaDiosLoAyuda :-
+    forall(
+        madruga(Fulano),
+        (esDios(Ente),
+        loAyuda(Ente, Fulano))
+    ).
+
+diosLeDaPan(dini).
+diosLeDaPan(casciati).
+
+tieneDientes(casciati).
+tieneDientes(viotti).
+
+diosLeDaPanAlQueNoTieneDientes :-
+   forall(
+        diosLeDaPan(Fulano),
+        not(tieneDientes(Fulano))
+   ). 
